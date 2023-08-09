e4ee2c3d7778f60656f7be73603bdb5fb0845bb6
Nodd <124269320+Indio01@users.noreply.github.com>
2023-08-09 13:36:24 +0000

    api teste

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Pressable, Picker} from 'react-native';
-import {Picker} from '@react-native-picker/picker'
+import { StyleSheet, Text, View, TextInput, Pressable} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 export default function App() {
 
-  const [moedaOrigem, setMoedaOrigem] = useState('')
-  const [moedaDestino, setMoedaDestino] = useState('')
-  
+  const [moedaOrigem, setMoedaOrigem] = useState('BRL')
+  const [moedaDestino, setMoedaDestino] = useState('USD')
+  const [valorConvertido, setValorConvertido] = useState('')
+
+  const buscarHandle= () =>{
+    let URL = `https://economia.awesomeapi.com.br/last/USD-BRL`
+    setValorConvertido(URL);
+  }
   return (
     <View style={styles.container}>
       <View>
@@ -15,8 +20,10 @@ export default function App() {
           selectedValue={moedaOrigem}
           onValueChange={(itemValue, itemIndex) => setMoedaOrigem(itemValue)}
         >
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
+          <Picker.Item label="Real Brasileiro" value="BRL" />
+          <Picker.Item label="Dolar Americano" value="USD" />
+          <Picker.Item label="Ouro" value="XAU" />
+          <Picker.Item label="Bitcoin" value="BTC" />
         </Picker>
       </View>
 
@@ -26,8 +33,10 @@ export default function App() {
           selectedValue={moedaDestino}
           onValueChange={(itemValue, itemIndex) => setMoedaDestino(itemValue)}
         >
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
+          <Picker.Item label="Real Brasileiro" value="BRL" />
+          <Picker.Item label="Dolar Americano" value="USD" />
+          <Picker.Item label="Ouro" value="XAU" />
+          <Picker.Item label="Bitcoin" value="BTC" />
         </Picker>
       </View>
 
