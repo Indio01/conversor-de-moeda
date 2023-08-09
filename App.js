eca9a12239206047c3a4195775cc78ae28d65f15
Nodd <124269320+Indio01@users.noreply.github.com>
2023-08-09 13:16:03 +0000

    teste inicial

--- App.js
+++ App.js
@@ -1,25 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Pressable} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable, Picker} from 'react-native';
+import {Picker} from '@react-native-picker/picker'
 
 export default function App() {
+
+  const [moedaOrigem, setMoedaOrigem] = useState('')
+  const [moedaDestino, setMoedaDestino] = useState('')
+  
   return (
     <View style={styles.container}>
       <View>
         <Text>Moeda 1</Text>
-        <View>
-          <TextInput/>
-        </View>
+        <Picker
+          selectedValue={moedaOrigem}
+          onValueChange={(itemValue, itemIndex) => setMoedaOrigem(itemValue)}
+        >
+          <Picker.Item label="Java" value="java" />
+          <Picker.Item label="JavaScript" value="js" />
+        </Picker>
       </View>
 
       <View>
         <Text>Moeda 2</Text>
-        <View>
-          <TextInput/>
-        </View>
+        <Picker
+          selectedValue={moedaDestino}
+          onValueChange={(itemValue, itemIndex) => setMoedaDestino(itemValue)}
+        >
+          <Picker.Item label="Java" value="java" />
+          <Picker.Item label="JavaScript" value="js" />
+        </Picker>
       </View>
 
       <View>
-          <Pressable>
+          <Pressable style={styles.botao}>
               <Text>Calcular</Text>
           </Pressable>
       </View>
@@ -37,5 +50,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    margin: 2,
+    width: 200,
+    textAlign: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 2,
   },
 });
