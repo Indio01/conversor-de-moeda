2fc0603d9012d24905cf7de3e1c0ded9c7b5d6ff
Nodd <124269320+Indio01@users.noreply.github.com>
2023-11-22 13:59:43 +0000

    conversor

--- App.js
+++ App.js
@@ -1,42 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Pressable} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
-import styles from './styles';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import styles from "./styles";
 
 export default function App() {
+  const [moedaOrigem, setMoedaOrigem] = useState("BRL");
+  const [moedaDestino, setMoedaDestino] = useState("USD");
+  const [valorConvertido, setValorConvertido] = useState("");
+  const [valorOriginal, setValorOriginal] = useState("33.33");
 
-  const [moedaOrigem, setMoedaOrigem] = useState('BRL')
-  const [moedaDestino, setMoedaDestino] = useState('USD')
-  const [valorConvertido, setValorConvertido] = useState('')
-  const [valorOriginal, setValorOriginal] = useState('33.33')
-
-  const buscarHandle= async () =>{
-    let URL = `https://economia.awesomeapi.com.br/last/${moedaOrigem}-${moedaDestino}`
+  const buscarHandle = async () => {
+    let URL = `https://economia.awesomeapi.com.br/last/${moedaOrigem}-${moedaDestino}`;
     try {
-      let page= await fetch(URL)
-      let json = await page.json()
-      let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high)
-      let valor= parseFloat(valorOriginal)
-      setValorConvertido((indice * valor).toFixed(2))    
-    } catch (error){
-      setValorConvertido(`Erro: ${error.message}`)
+      let page = await fetch(URL);
+      let json = await page.json();
+      let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high);
+      let valor = parseFloat(valorOriginal);
+      setValorConvertido((indice * valor).toFixed(2));
+    } catch (error) {
+      setValorConvertido(`Erro: ${error.message}`);
     }
   };
 
-  const limparResultado= () => {
-    setValorConvertido('')
-    setValorOriginal('33.33')
-    setMoedaOrigem('BRL')
-    setMoedaDestino('USD')
+  const limparResultado = () => {
+    setValorConvertido("");
+    setValorOriginal("33.33");
+    setMoedaOrigem("BRL");
+    setMoedaDestino("USD");
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>Conversor de Moedas</Text>
-      <View>
+      <View
+        style={{
+          height: 60,
+          width: 300,
+          backgroundColor: "#373737",
+          borderRadius: 5,
+          marginBottom: 10,
+          alignItems: "center",
+        }}
+      >
         <Picker
-          style={{height: 50, width: 300, color: "white", backgroundColor: "#373737", borderRadius: 20, marginBottom:10,}}
+          style={{
+            height: 40,
+            width: 290,
+            color: "white",
+            backgroundColor: "#373737",
+            borderRadius: 20,
+            marginBottom: 10,
+          }}
           selectedValue={moedaOrigem}
           onValueChange={(itemValue, itemIndex) => setMoedaOrigem(itemValue)}
         >
@@ -47,9 +62,25 @@ export default function App() {
         </Picker>
       </View>
 
-      <View>
+      <View
+        style={{
+          height: 60,
+          width: 300,
+          backgroundColor: "#373737",
+          borderRadius: 5,
+          marginBottom: 10,
+          alignItems: "center",
+        }}
+      >
         <Picker
-          style={{height: 50, width: 300, color: "white", backgroundColor: "#373737", borderRadius: 5, marginBottom:10,}}
+          style={{
+            height: 40,
+            width: 290,
+            color: "white",
+            backgroundColor: "#373737",
+            borderRadius: 5,
+            marginBottom: 10,
+          }}
           selectedValue={moedaDestino}
           onValueChange={(itemValue, itemIndex) => setMoedaDestino(itemValue)}
         >
@@ -61,19 +92,34 @@ export default function App() {
       </View>
 
       <View style={styles.viewInput}>
-        <TextInput style={styles.input} value={valorOriginal} onChangeText={setValorOriginal} keyboardType='numeric'/>
+        <TextInput
+          style={styles.input}
+          value={valorOriginal}
+          onChangeText={setValorOriginal}
+          keyboardType="numeric"
+        />
       </View>
 
       <View style={styles.item}>
-          <Pressable style={styles.botao} onPress={buscarHandle}>
-              <Text style={styles.texto}>Buscar Valor</Text>
-          </Pressable>
-          <Pressable style={styles.botao} onPress={limparResultado}>
-              <Text style={styles.texto}>Limpar</Text>
-          </Pressable>
-          <View style={styles.botaoResultado}>
-            <Text style={styles.texto}>{`Resultado: ${valorConvertido}`}</Text>
-          </View>
+        <Pressable style={styles.botaoLimpar} onPress={limparResultado}>
+          <Text style={styles.texto}>Limpar</Text>
+        </Pressable>
+        <Pressable style={styles.botao} onPress={buscarHandle}>
+          <Text style={styles.texto}>Buscar Valor</Text>
+        </Pressable>
+        <View style={styles.botaoResultado}>
+          <Text
+            style={{
+              color: "#373737",
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: 15,
+              paddingBottom: 10,
+            }}
+          >
+            {`Resultado: ${valorConvertido}`}
+          </Text>
+        </View>
       </View>
     </View>
   );
